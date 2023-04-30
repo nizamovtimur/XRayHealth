15d644bd7db6843df076016ca637ff64c84f072c
nizamovtimur <abc@nizamovtimur.ru>
2023-04-30 19:15:40 +0500

    Drop files from .gitignore

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ load_dotenv()
 
 
 class Config:
-    MONGO_URI = environ.get("MONGO_URI")
-    SECRET_KEY = environ.get("SECRET_KEY")
+    MONGO_URI = environ.get("MONGO_URI") or "mongodb://localhost:27017/xrayhealth"
+    SECRET_KEY = environ.get("SECRET_KEY") or "secret"
