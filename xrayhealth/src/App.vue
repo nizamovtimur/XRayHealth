c31847b370dd8e3242ee6335997b9fb3775f4b08
dimas <89324713054@mail.ru>
2023-05-14 14:18:16 +0500

    header processing

--- xrayhealth/src/App.vue
+++ xrayhealth/src/App.vue
@@ -3,16 +3,40 @@ import { RouterLink, RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-  </header>
+    <header>
+        <div class="container">
+            <img src="./assets/logo.svg" width="24" height="24" alt="logo">
+            <nav>
+              <RouterLink to="/" class="nav-item">Главная</RouterLink>
+              <RouterLink to="/about" class="nav-item">About</RouterLink>
+            </nav>
+        </div>
+    </header>
 
-  <RouterView />
+    <div class="container">
+        <RouterView />
+    </div>
 </template>
 
 <style scoped>
-
+    .container
+    {
+        width: 100%;
+        display: flex;
+        justify-content: space-between;
+        margin: 0 auto;
+        max-width: var(--vp-screen-max-width);
+    }
+    header
+    {
+        display: flex;
+        border-bottom: 1px solid var(--vt-c-divider-light-2);
+        padding: 0 32px;
+        height: var(--vt-nav-height);
+        align-items: center;
+    }
+    .nav-item
+    {
+        padding: 12px;
+    }
 </style>
